37532b306ab92542e77dc32b975ec4a803e9f349
Ali Toosi <alitoosid80@gmail.com>
2022-01-20 20:28:37 +0330

    Add most_visit_URLs function and menu

--- script.sh
+++ script.sh
@@ -9,12 +9,13 @@ echo Analyzing logs from "$log" file
 while true
 do
     echo    "1 Show IPs"
+    echo    "2 most_visit_URLs"
     
     read -p "select number: " selection
     
     case $selection in
-    1) test;;
-	
+    1) showIPs;;
+	2) most_visit_URLs;;
 
     esac
     
@@ -42,6 +43,12 @@ function showIPs {
     cat $log | awk -F\" '{ print $1 }'| wc | awk '{print "All of IPs are: " $1}'
     cat $log | awk '{ print count "times {" $1 "} IP is repeated."}' | sort -r | uniq -c | sort -r
     cat $log | awk '{ print $1}' | uniq > allIPs.txt
+}
+
+function most_visit_URLs {
+    echo 10 Most URLs Visited:
+    awk '{count[$7]++} END {for (url in count) print url, count[url]}' $log | sort -k 2nr | head -n 10
+    awk '{count[$7]++} END {for (url in count) print url, count[url]}' $log | sort -k 2nr | head -n 10 > MostVisitedURLs.txt
 }
 
 input
