10d28fdf10c9299450fe4e916e0d48333d103ecc
Ali Toosi <alitoosid80@gmail.com>
2022-01-20 21:06:01 +0330

    Add comments for more details

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+#Ali Toosi - 9821123
+#Apache Analyzer Project
+
 clear
 
 function menu {
 
-echo Analyzing logs from "$log" file
+echo Analyzing from "$log" file
 
 while true
 do
@@ -43,7 +46,7 @@ else
 fi
 }
 
-
+#Function is show all IPs.
 function showIPs {
     cat $log | awk '{ print $1}' | sort | uniq | wc | awk '{print $1 " IPs without repetition found" }'
     cat $log | awk -F\" '{ print $1 }'| wc | awk '{print "All of IPs are: " $1}'
@@ -51,12 +54,14 @@ function showIPs {
     cat $log | awk '{ print $1}' | uniq > allIPs.txt
 }
 
+#Function is show most visit URLs when URL is called.
 function most_visit_URLs {
     echo 5 Most URLs Visited:
     awk '{count[$7]++} END {for (url in count) print url, count[url]}' $log | sort -k 2nr | head -n 5
     awk '{count[$7]++} END {for (url in count) print url, count[url]}' $log | sort -k 2nr | head -n 5 > MostVisitedURLs.txt
 }
 
+#Function show top IPs that most search with user.
 function topIPs {
     cat $log | awk '{ print $1}' | sort | uniq | wc | awk '{print $1 " IPs without repetition found" }'
     cat $log | awk -F\" '{ print $1 }'| wc | awk '{print "All of IPs are: " $1}'
@@ -65,6 +70,7 @@ function topIPs {
     awk '{print count "times {" $1 "} IP is repeated."}' $log | sort | uniq -c  | sort -nr | head -n 10 > TopIPs.txt
 }
 
+#Function show popular browser that user most search in.
 function popular_browser {
     awk '{brow[$(NF)]++} END {for (item in brow) print item, brow[item]}' $file | sort -k 2nr | head -n 10 > "$file1"popularBrowsers.txt
     figlet Result
@@ -72,6 +78,7 @@ function popular_browser {
     printf '1- *popularBrowsers.txt\n\n'
 }
 
+#Function show what OS from visitors most used.
 function maxvisitorsOS {
     echo Top 5 visitors Opeation Systems:
     awk '{count[$13]++} END {for (os in count) print os, count[os]}' $log | sort -k 2nr | head -n 5
