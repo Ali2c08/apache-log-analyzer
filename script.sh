720fef2032828efeea0e1d5e15d60983ed1d6994
Ali Toosi <alitoosid80@gmail.com>
2022-01-20 20:19:15 +0330

    Add shoeIPs function and menu

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ echo Analyzing logs from "$log" file
 
 while true
 do
-    echo    "|1 Show All IPs                          |"
+    echo    "1 Show IPs"
     
     read -p "select number: " selection
     
@@ -22,14 +22,14 @@ done
 }
 
 function input {
-echo -e "Please select the input log:"
+echo -e "Please select log:"
 ls -p | grep -w log
 echo
 read log
 
 if [ ! -f $log ]; then
     echo -e "\nFile not found!\n"
-    echo -e "Please try again and make sure log file is available in directory"
+    echo -e "Please try again"
     getFile
 else
     menu
@@ -37,7 +37,11 @@ fi
 }
 
 
-function test {
-	echo hi
+function showIPs {
+    cat $log | awk '{ print $1}' | sort | uniq | wc | awk '{print $1 " IPs without repetition found" }'
+    cat $log | awk -F\" '{ print $1 }'| wc | awk '{print "All of IPs are: " $1}'
+    cat $log | awk '{ print count "times {" $1 "} IP is repeated."}' | sort -r | uniq -c | sort -r
+    cat $log | awk '{ print $1}' | uniq > allIPs.txt
 }
+
 input
